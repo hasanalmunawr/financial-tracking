0648e92d081ee0833d4e89446899276aec430e34
hasan almunawar <109322433+hasanalmunawr@users.noreply.github.com>
2025-05-29 19:37:15 +0700

    first commit

---
+++ database/migrations/2025_05_29_122712_create_transactions_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('transactions', function (Blueprint $table) {
+            $table->id();
+            $table->foreign('category_id')->references('id')->on('categories')->onDelete('cascade');
+            $table->float('amount');
+            $table->text('description')->nullable();
+            $table->date('date');
+            $table->string('type');
+            $table->string('note')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('transactions');
+    }
+};
