4b2baaeb2f564762c441eb45265942f24ad40f26
hasan almunawar <hasanalmunawar01@gmail.com>
2025-05-30 09:05:16 +0700

    09:05

--- database/migrations/2025_05_29_122457_create_categories_table.php
+++ database/migrations/2025_05_29_122457_create_categories_table.php
@@ -14,8 +14,7 @@ return new class extends Migration
         Schema::create('categories', function (Blueprint $table) {
             $table->id();
             $table->string('name');
-            $table->string('type')->type('income', 'expense');
-
+            $table->enum('type', ['income', 'expense']);
             $table->timestamps();
         });
     }
--- database/migrations/2025_05_29_122712_create_transactions_table.php
+++ database/migrations/2025_05_29_122712_create_transactions_table.php
@@ -13,7 +13,7 @@ return new class extends Migration
     {
         Schema::create('transactions', function (Blueprint $table) {
             $table->id();
-            $table->foreign('category_id')->references('id')->on('categories')->onDelete('cascade');
+            $table->bigInteger('category_id')->nullable();
             $table->float('amount');
             $table->text('description')->nullable();
             $table->date('date');
